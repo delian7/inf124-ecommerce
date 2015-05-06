90aa20f9f1c5d4f2e4338a91fd1903a3a2d6efd8
michaeljaeoh <mjoh@uci.edu>
2015-05-05 20:10:04 -0700

    Added Rotating Image to home page
    Pretty easy to add/change pictures if you don’t like them. if you want
    to add or change any pictures, just find and save whatever picture as a
    .jpg and give it numerical name. (i.e. 1, 2, 3, …etc)

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
   <title>Food2U</title>
   <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
   <link rel="stylesheet" type="text/css" href="css/default.css" />
+  <script type="text/javascript" src="assets/javascript/rotate.js"></script>
 </head>
 
-<body>
+<body onload="startRotation()">
 
   <div id="page">
 
@@ -15,11 +16,13 @@
     <div class="container">
       <div id="main">
         <h2 class="center">Welcome to Food2U</h3>
+          <div id="image" align="center">
+            <img id="placeholder" src="" alt="Food Delivered" width="250" height="150"/></div>
+
         <h3 class="center">Where UCI students deliver food to other students.</h3>
         <p>Just click your favorite restaurant, from the list below and then click order. A UCI student will then contact you and deliver the Food2U.</p><br />
 
         <div class="grid-container outline">
-
 
           <div class="row">
             <a href="in-n-out.php">
